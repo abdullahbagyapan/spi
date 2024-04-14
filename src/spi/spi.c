967aadd4cddb41ccb2f3923e9cec3b30f340ee2e
Abdullah Bagyapan <abdullahbagyapan@hotmail.com>
2024-04-14 19:33:57 +0300

    feat::add ring buffer:spi
    -Implement ring buffer
    
    -Add functions
    --SPI_RingBufferGet()
    --SPI_RingBufferPut()
    --SPI_RingBufferFull()
    --SPI_RingBufferEmpty()
    
    -Add interrupt function

--- src/spi/spi.c
+++ src/spi/spi.c
@@ -12,9 +12,19 @@
 
 // AVR libraries
 #include <avr/io.h>
+#include <avr/interrupt.h>
 
 // Standart libraries
 #include <stdint.h>
+
+
+
+/*================================== Defined Variables ==================================*/
+
+
+static struct SPI_RING_BUFFER _spi_tx_ring_buffer = {.ui8Head = 0, .ui8Tail = 0};
+static struct SPI_RING_BUFFER _spi_rx_ring_buffer = {.ui8Head = 0, .ui8Tail = 0};
+
 
 
 /*================================== Functions ==================================*/
@@ -72,4 +82,76 @@ uint8_t SPI_Receive() {
 
     return SPDR;
 
+}
+
+
+uint8_t SPI_RingBufferGet(struct SPI_RING_BUFFER *pRingBuffer) {
+
+    const uint8_t ui8data = pRingBuffer->buffer[pRingBuffer->ui8Tail];
+    pRingBuffer->ui8Tail++;
+
+    // if tail points end of the buffer, set tail to index 0
+    if (pRingBuffer->ui8Tail == SPI_RING_BUFFER_SIZE) {
+        pRingBuffer->ui8Tail = 0;
+    }
+
+    return ui8data;
+}
+
+
+void SPI_RingBufferPut(struct SPI_RING_BUFFER *pRingBuffer, uint8_t ui8Data) {
+    
+    pRingBuffer->buffer[pRingBuffer->ui8Head] = ui8Data;
+    pRingBuffer->ui8Head++;
+
+    // if head points end of the buffer, set head to index 0
+    if (pRingBuffer->ui8Head == SPI_RING_BUFFER_SIZE) {
+        pRingBuffer->ui8Head = 0;
+    }
+}
+
+
+uint8_t SPI_RingBufferFull(const struct SPI_RING_BUFFER *pRingBuffer) {
+
+    uint8_t ui8IsRingBufferFull;
+
+    uint8_t ui8IndexAfterHead = pRingBuffer->ui8Head;
+
+    // if next index of head points end of the buffer, set it to index 0
+    if (ui8IndexAfterHead == SPI_RING_BUFFER_SIZE) {
+        ui8IndexAfterHead = 0;
+    }
+
+    // if next index of head equals tail, there is no space for more character
+    ui8IsRingBufferFull = ui8IndexAfterHead == pRingBuffer->ui8Tail;
+
+    return ui8IsRingBufferFull;
+}
+
+
+uint8_t SPI_RingBufferEmpty(const struct SPI_RING_BUFFER *pRingBuffer) {
+
+    uint8_t ui8IsRingBufferEmpty;
+
+    // if head index and tail index same, there is no characters to put ring buffer before
+    ui8IsRingBufferEmpty = pRingBuffer->ui8Head == pRingBuffer->ui8Tail;
+
+    return ui8IsRingBufferEmpty;
+
+}
+
+
+
+/************ Interrupt Handler Functions ************/
+
+
+// SPI Serial Transfer Complete
+// If a data received, put it into rx buffer
+ISR(SPI_STC_vect)
+{
+
+    uint8_t ui8Data = SPDR;
+
+    SPI_RingBufferPut(&_spi_rx_ring_buffer,ui8Data);
+    
 }
