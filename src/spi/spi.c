e47c48549dfa771ceb454d40880bacc2b753766d
Abdullah Bagyapan <abdullahbagyapan@hotmail.com>
2024-04-15 13:25:29 +0300

    remove::ring buffer:spi

--- src/spi/spi.c
+++ src/spi/spi.c
@@ -7,11 +7,11 @@
 
 /*================================== Libraries ==================================*/
 
+
 // Module's own header
 #include "spi.h"
 
 // AVR libraries
-#include <avr/io.h>
 #include <avr/interrupt.h>
 
 // Standart libraries
@@ -19,50 +19,7 @@
 
 
 
-/*================================== Defined Variables ==================================*/
-
-
-static struct SPI_RING_BUFFER _spi_tx_ring_buffer = {.ui8Head = 0, .ui8Tail = 0};
-static struct SPI_RING_BUFFER _spi_rx_ring_buffer = {.ui8Head = 0, .ui8Tail = 0};
-
-
-
 /*================================== Functions ==================================*/
-
-
-
-/************ Private Functions ************/
-
-
-/*
- @brief:  Start SPI transmission
-
- @author  Abdullah Bagyapan
-
- @date    14/04/2024
-
- @param   None
-
- @details Get data from tx ring buffer, then put it into TX register
-
- @return  None
-*/
-static void _SPI_TX_Start(void) {
-
-    uint8_t ui8Data;
-
-    ui8Data = SPI_RingBufferGet(&_spi_tx_ring_buffer);
-
-    // Start transmission
-    SPDR = ui8Data;
-
-    // Wait for transmission complete
-    while(!(SPSR & _BV(SPIF)));
-
-}
-
-
-/************ Public Functions ************/
 
 
 
@@ -80,6 +37,9 @@ void SPI_MasterInit() {
 
     // Set Clock Polarity and Clock Phase
     SPCR &= ~_BV(CPOL) | ~_BV(CPHA);
+
+    // Set chip select HIGH
+    PORTB |=  _BV(SPI_CS_PIN);
     
 }
 
@@ -101,15 +61,11 @@ void SPI_SlaveInit() {
 
 void SPI_PutChar(uint8_t ui8Data) {
 
-    const uint8_t ui8IsTXOngoing = !SPI_RingBufferEmpty(&_spi_tx_ring_buffer);
+    // Start transmission
+    SPDR = ui8Data;
 
-    SPI_RingBufferPut(&_spi_tx_ring_buffer,ui8Data);
-
-    // if transmit is not happening, start transmitting
-    if (!ui8IsTXOngoing) {
-
-        _SPI_TX_Start();
-    }
+    // Wait for transmission complete
+    while(!(SPSR & _BV(SPIF)));
 
 }
 
@@ -134,76 +90,4 @@ uint8_t SPI_GetChar() {
 
     return SPDR;
 
-}
-
-
-uint8_t SPI_RingBufferGet(struct SPI_RING_BUFFER *pRingBuffer) {
-
-    const uint8_t ui8data = pRingBuffer->buffer[pRingBuffer->ui8Tail];
-    pRingBuffer->ui8Tail++;
-
-    // if tail points end of the buffer, set tail to index 0
-    if (pRingBuffer->ui8Tail == SPI_RING_BUFFER_SIZE) {
-        pRingBuffer->ui8Tail = 0;
-    }
-
-    return ui8data;
-}
-
-
-void SPI_RingBufferPut(struct SPI_RING_BUFFER *pRingBuffer, uint8_t ui8Data) {
-    
-    pRingBuffer->buffer[pRingBuffer->ui8Head] = ui8Data;
-    pRingBuffer->ui8Head++;
-
-    // if head points end of the buffer, set head to index 0
-    if (pRingBuffer->ui8Head == SPI_RING_BUFFER_SIZE) {
-        pRingBuffer->ui8Head = 0;
-    }
-}
-
-
-uint8_t SPI_RingBufferFull(const struct SPI_RING_BUFFER *pRingBuffer) {
-
-    uint8_t ui8IsRingBufferFull;
-
-    uint8_t ui8IndexAfterHead = pRingBuffer->ui8Head;
-
-    // if next index of head points end of the buffer, set it to index 0
-    if (ui8IndexAfterHead == SPI_RING_BUFFER_SIZE) {
-        ui8IndexAfterHead = 0;
-    }
-
-    // if next index of head equals tail, there is no space for more character
-    ui8IsRingBufferFull = ui8IndexAfterHead == pRingBuffer->ui8Tail;
-
-    return ui8IsRingBufferFull;
-}
-
-
-uint8_t SPI_RingBufferEmpty(const struct SPI_RING_BUFFER *pRingBuffer) {
-
-    uint8_t ui8IsRingBufferEmpty;
-
-    // if head index and tail index same, there is no characters to put ring buffer before
-    ui8IsRingBufferEmpty = pRingBuffer->ui8Head == pRingBuffer->ui8Tail;
-
-    return ui8IsRingBufferEmpty;
-
-}
-
-
-
-/************ Interrupt Handler Functions ************/
-
-
-// SPI Serial Transfer Complete
-// If a data received, put it into rx buffer
-ISR(SPI_STC_vect)
-{
-
-    uint8_t ui8Data = SPDR;
-
-    SPI_RingBufferPut(&_spi_rx_ring_buffer,ui8Data);
-    
 }
--- src/spi/spi.h
+++ src/spi/spi.h
@@ -12,18 +12,6 @@
 #include <stdint.h>
 
 
-/*================================== Defined Variables ==================================*/
-
-
-#define SPI_RING_BUFFER_SIZE    255
-
-struct SPI_RING_BUFFER
-{
-    uint8_t buffer[SPI_RING_BUFFER_SIZE];
-    uint8_t ui8Head;
-    uint8_t ui8Tail;
-};
-
 
 /*================================== Functions ==================================*/
 
@@ -110,62 +98,3 @@ void SPI_PutChar(uint8_t ui8Data);
  @return  None
 */
 void SPI_PutString(uint8_t *ui8pData);
-
-
-
-/*
- @brief:  Get character from ring buffer
-
- @author  Abdullah Bagyapan
-
- @date    14/04/2024
-
- @param   pRingBuffer, the ring buffer
-
- @return  The character from ring buffer
-*/
-uint8_t SPI_RingBufferGet(struct SPI_RING_BUFFER *pRingBuffer);
-
-
-
-/*
- @brief:  Put character to ring buffer
-
- @author  Abdullah Bagyapan
-
- @date    14/04/2024
-
- @param   pRingBuffer, the ring buffer
- @param   ui8Data    , the character to put into ring buffer
-
- @return  None
-*/
-void SPI_RingBufferPut(struct SPI_RING_BUFFER *pRingBuffer, uint8_t ui8Data);
-
-
-/*
- @brief:  Check if ring buffer is full or not
-
- @author  Abdullah Bagyapan
-
- @date    14/04/2024
-
- @param   pRingBuffer, the ring buffer
-
- @return  If ring buffer is not full return 0, otherwise 1 
-*/
-uint8_t SPI_RingBufferFull(const struct SPI_RING_BUFFER *pRingBuffer);
-
-
-/*
- @brief:  Check if ring buffer is empty or not
-
- @author  Abdullah Bagyapan
-
- @date    14/04/2024
-
- @param   pRingBuffer, the ring buffer
-
- @return  If ring buffer is not empty return 0, otherwise 1 
-*/
-uint8_t SPI_RingBufferEmpty(const struct SPI_RING_BUFFER *pRingBuffer);
