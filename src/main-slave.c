52b27ac366a8bad438b65019720eaa6e62a0a02a
Abdullah Bagyapan <abdullahbagyapan@hotmail.com>
2024-04-20 17:31:51 +0300

    feat(main):import UART module

--- src/main-slave.c
+++ src/main-slave.c
@@ -1,16 +1,18 @@
 #include "spi.h"
+#include "uart.h"
 
 
 int main(void) {
 
     SPI_SlaveInit();
-
-    
+    UART_Init();
 
     while (1) {
 
         uint8_t ui8Data = SPI_GetChar();
-
+        
+        UART_PutChar(ui8Data);
+        
     }
 
     return 0;
