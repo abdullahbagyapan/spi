aeaa98daef501e9dd2146d26ad1f81034cb685da
Abdullah Bagyapan <abdullahbagyapan@hotmail.com>
2024-04-21 13:55:11 +0300

    fix(main-master):delete led & clock functions

--- src/main-master.c
+++ src/main-master.c
@@ -1,29 +1,18 @@
 #include "spi.h"
-#include "led.h"
-#include "clock.h"
 
 #include "avr/interrupt.h"
 
 
 int main(void) {
 
-    CLOCK_Init();
-    LED_Init();
     SPI_MasterInit();
-
-    sei();
 
     SPI_BeginTransaction();
 
     while (1) {
 
-        LED_On();
-        CLOCK_DelayMs(1000);
-
         SPI_PutChar('A');
             
-        LED_Off();
-        CLOCK_DelayMs(1000);    
     }
 
     return 0;
